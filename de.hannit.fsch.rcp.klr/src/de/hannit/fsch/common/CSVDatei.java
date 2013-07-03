f5ffc338be42dd9b5a21c62d9b2b23d2095c11a0
frankschaare <frank.schaare@gmail.com>
2013-07-03 14:57:19 +0200

    CSVDatei integriert

---
+++ de.hannit.fsch.rcp.klr/src/de/hannit/fsch/common/CSVDatei.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package de.hannit.fsch.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fsch
+ * @since 14.06.2013
+ *
+ */
+public class CSVDatei extends File 
+{
+private Charset charset = Charset.forName("ISO-8859-1");
+private List<String> lines;
+private ArrayList<String[]> fields = new ArrayList<String[]>();	
+
+private boolean hasHeader = true;
+private String delimiter = ";";
+private int lineCount = -1;
+
+	/**
+	 * @param arg0
+	 */
+	public CSVDatei(String arg0) 
+	{
+	super(arg0);
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public CSVDatei(URI arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public CSVDatei(String arg0, String arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public CSVDatei(File arg0, String arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void read() 
+	{
+	lineCount = 1;
+		try 
+		{
+		lines = Files.readAllLines(Paths.get(super.getPath()), charset);
+			for (String line : lines)
+			{
+			switch (lineCount) 
+			{
+			// Erste Zeile wird nur verarbeitet wenn keine Kopfzeile vorhanden ist
+			case 1:
+				if (!hasHeader) 
+				{
+				fields.add(line.split(delimiter));	
+				}
+			break;
+
+			default:
+			fields.add(line.split(delimiter));
+			break;
+			}	
+			lineCount++;
+			}	
+		} 
+		catch (IOException e) 
+		{
+		e.printStackTrace();	
+		}
+		
+	}
+	
+	
+	public List<String> getLines() 
+	{
+	return lines;
+	}
+
+	public ArrayList<String[]> getFields() 
+	{
+		return fields;
+	}
+
+	public boolean isHeader() {
+		return hasHeader;
+	}
+
+	public void hasHeader(boolean hasHeader) 
+	{
+		this.hasHeader = hasHeader;
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public int getLineCount() {
+		return lineCount;
+	}
+
+	public void setLineCount(int lineCount) {
+		this.lineCount = lineCount;
+	}
+	
+	
+
+}
